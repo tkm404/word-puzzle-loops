e21f4f3f2b3c1d881976ed31a5941a9efe936be5
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-03-29 11:23:14 -0700

    add index.html and scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+window.onload = function(event) {
+  event.preventDefault();
+  const div = document.createElement("div")
+  document.querySelector("body").append(div)
+  const p = document.createElement("p");
+  document.querySelector("div").append(p);
+  p.append("Can you solve the riddle?");
+  const ul = document.createElement("ul");
+  div.append(ul);
+  const liFirst = document.createElement("li");
+  const liSecond = document.createElement("li");
+  const liThird = document.createElement("li");
+  ul.append(liFirst, liSecond, liThird);
+	liFirst.append("Believe you can and you're halfway there. Theodore Roosevlet");
+	liSecond.append("Knowledge is Power. Francis Bacon");
+	liThird.append("Be the change that you wish to see in the world. Mahatma Ghandi");
+}
+
